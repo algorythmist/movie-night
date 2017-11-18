893da66a6cc0c7eb3eadc5ab901273f44faaaed8
Dimitri Papaioannou <dimitri@trizic.com>
2017-11-18 11:26:42 -0800

    intro to Spring

---
+++ movie-spring/src/main/java/com/tecacet/movie/service/ImmutableDirector.java
@@ -0,0 +1,67 @@
+package com.tecacet.movie.service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.tecacet.movie.domain.Director;
+import com.tecacet.movie.domain.Movie;
+
+public class ImmutableDirector implements Director {
+
+	private final String name;
+	private final double rating;
+	private final List<? extends Movie> movies;
+	private final Set<String> genres;
+
+	public ImmutableDirector(String name, double rating, List<? extends Movie> movies, Set<String> genres) {
+		super();
+		this.name = name;
+		this.rating = Math.round(rating * 100) / 100.;
+		this.movies = movies;
+		this.genres = new TreeSet<>(genres);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public double getRating() {
+		return rating;
+	}
+
+	@Override
+	public Set<String> getGenres() {
+		return genres;
+	}
+
+	@Override
+	public List<? extends Movie> getMovies() {
+		return movies;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof ImmutableDirector)) {
+			return false;
+		}
+		Director director = (Director) other;
+		return name.equals(director.getName());
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: Rating = %.2f. Movies = %d. Genres = %s", getName(), getRating(),
+				getMovies().size(), getGenres());
+	}
+}
---
+++ movie-spring/src/test/java/com/tecacet/movie/service/ImmutableDirectorTest.java
@@ -0,0 +1,37 @@
+package com.tecacet.movie.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.tecacet.movie.domain.Director;
+import com.tecacet.movie.domain.Movie;
+
+public class ImmutableDirectorTest {
+
+	@Test
+	public void testEqualsHashCodeToString() {
+		List<? extends Movie> movies = new ArrayList<>();
+		Set<String> genres = new HashSet<>(Arrays.asList("Drama", "Comedy"));
+		Director director1 = new ImmutableDirector("Testonius", 7.3, movies, genres);
+		assertEquals("Testonius: Rating = 7.30. Movies = 0. Genres = [Comedy, Drama]", director1.toString());
+
+		Director director2 = new ImmutableDirector("Testonius2", 7.3, movies, genres);
+		assertNotEquals(director1, director2);
+		assertFalse(director1.hashCode() == director2.hashCode());
+		
+		Director director3 = new ImmutableDirector("Testonius", 0, new ArrayList<>(), new HashSet<>());
+		assertEquals(director1, director3);
+		assertTrue(director1.hashCode() == director3.hashCode());
+	}
+
+}
