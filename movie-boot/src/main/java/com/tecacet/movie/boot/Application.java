b5f500c424da2ca8fca2b632f3092f3ca39472c7
Dimitri Papaioannou <dimitri@trizic.com>
2017-11-19 19:26:35 -0800

    jmx example

--- movie-boot/src/main/java/com/tecacet/movie/boot/Application.java
+++ movie-boot/src/main/java/com/tecacet/movie/boot/Application.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 import com.tecacet.movie.jpa.config.PersistenceConfiguration;
 
 @SpringBootApplication
 @ImportAutoConfiguration(classes = PersistenceConfiguration.class)
 @EnableAspectJAutoProxy
+@EnableMBeanExport
 public class Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
