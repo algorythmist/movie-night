0fa929c3f82e1c49a002d6beca30d42092d3c152
dimitri <dimitri@trizic.com>
2017-11-26 09:34:25 -0800

    removed TODO

--- movie-jpa/src/main/java/com/tecacet/movie/jpa/repository/MovieDirectorRepository.java
+++ movie-jpa/src/main/java/com/tecacet/movie/jpa/repository/MovieDirectorRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.tecacet.movie.jpa.model.EntityMovie;
 import com.tecacet.movie.jpa.model.MovieDirector;
 
-//TODO make read-only repository
 public interface MovieDirectorRepository extends Repository<MovieDirector, Long> {
 
 	@Query("select mp.movie from MovieDirector mp where mp.director.name = :name")
