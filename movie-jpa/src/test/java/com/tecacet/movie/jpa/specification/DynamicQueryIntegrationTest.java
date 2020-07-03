45d50f50a36dc7033d6943b0732b7411e13f0da5
algorythmist <dimitri@tecacet.com>
2020-07-03 17:57:11 +0000

    formatting

--- movie-jpa/src/test/java/com/tecacet/movie/jpa/specification/DynamicQueryIntegrationTest.java
+++ movie-jpa/src/test/java/com/tecacet/movie/jpa/specification/DynamicQueryIntegrationTest.java
@@ -2,11 +2,11 @@ package com.tecacet.movie.jpa.specification;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.transaction.Transactional;
+import com.tecacet.movie.jpa.config.PersistenceConfiguration;
+import com.tecacet.movie.jpa.model.EntityMovie;
+import com.tecacet.movie.jpa.repository.MovieRepository;
+import com.tecacet.movie.jpa.service.DatabasePopulator;
+import com.tecacet.movie.jpa.specification.SearchCriteria.Operation;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,44 +18,44 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.tecacet.movie.jpa.config.PersistenceConfiguration;
-import com.tecacet.movie.jpa.model.EntityMovie;
-import com.tecacet.movie.jpa.repository.MovieRepository;
-import com.tecacet.movie.jpa.service.DatabasePopulator;
-import com.tecacet.movie.jpa.specification.SearchCriteria.Operation;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.transaction.Transactional;
 
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { PersistenceConfiguration.class })
+@ContextConfiguration(classes = {PersistenceConfiguration.class})
 @Transactional
 public class DynamicQueryIntegrationTest {
-	
-	@Autowired
-	private MovieRepository movieRepository;
 
-	@Autowired
-	private DatabasePopulator databasePopulator;
+    @Autowired
+    private MovieRepository movieRepository;
 
-	private AtomicBoolean loaded = new AtomicBoolean(false);
+    @Autowired
+    private DatabasePopulator databasePopulator;
 
-	@Before
-	public void setUpDatabase() throws IOException {
-		if (!loaded.getAndSet(true)) {
-			databasePopulator.loadMovies();
-		}
-	}
+    private final AtomicBoolean loaded = new AtomicBoolean(false);
 
-	@Test
-	public void dynamicQuery() {
-		MovieSpecification olderMovies =
-				new MovieSpecification(new SearchCriteria("year", Operation.LESS, 1990));
-		MovieSpecification goodRating =
-				new MovieSpecification(new SearchCriteria("rating", Operation.GREATER, 8.0));
-		PageRequest pageRequest =  PageRequest.of(0, 10, Sort.by("title").descending());
-		Page<EntityMovie> page = movieRepository.findAll(olderMovies.and(goodRating), pageRequest);
-		assertEquals(10, page.getNumberOfElements());
-		List<EntityMovie> oldiesButGoldies = page.getContent();
-		assertEquals("The Wizard of Oz", oldiesButGoldies.get(0).getTitle());
-		assertEquals("Il buono, il brutto, il cattivo.", oldiesButGoldies.get(9).getTitle());
-	}
+    @Before
+    public void setUpDatabase() throws IOException {
+        if (!loaded.getAndSet(true)) {
+            databasePopulator.loadMovies();
+        }
+    }
+
+    @Test
+    public void dynamicQuery() {
+        MovieSpecification olderMovies =
+                new MovieSpecification(new SearchCriteria("year", Operation.LESS, 1990));
+        MovieSpecification goodRating =
+                new MovieSpecification(new SearchCriteria("rating", Operation.GREATER, 8.0));
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("title").descending());
+        Page<EntityMovie> page = movieRepository.findAll(olderMovies.and(goodRating), pageRequest);
+        assertEquals(10, page.getNumberOfElements());
+        List<EntityMovie> oldiesButGoldies = page.getContent();
+        assertEquals("The Wizard of Oz", oldiesButGoldies.get(0).getTitle());
+        assertEquals("Il buono, il brutto, il cattivo.", oldiesButGoldies.get(9).getTitle());
+    }
 }
